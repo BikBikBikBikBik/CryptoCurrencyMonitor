31cfc5c50d440e33835ffd9cf74dfaf898823f43
BikBikBikBikBik <BikBikBikBikBik@users.noreply.github.com>
2017-07-05 00:37:03 -0600

    Refactored currency selection form to improve speed of transferring currencies between lists

--- CryptoCurrencyMonitor.MarketData.Monitor/CurrencySelectionForm.cs
+++ CryptoCurrencyMonitor.MarketData.Monitor/CurrencySelectionForm.cs
@@ -9,7 +9,7 @@ namespace CryptoCurrencyMonitor.MarketData.Monitor {
 		public CurrencySelectionForm(ICollection<int> selectedCurrencyTypes) {
 			InitializeComponent();
 
-			_selectedCurrencyTypes = selectedCurrencyTypes;
+			_selectedCurrencyTypes = (IReadOnlyCollection<int>)selectedCurrencyTypes;
 
 			InitializeCurrencyLists();
 		}
@@ -24,39 +24,50 @@ namespace CryptoCurrencyMonitor.MarketData.Monitor {
 			_unselectedCurrenciesDataSource = new BindingList<Currency>(currencySelectionGrouping.Where(g => !g.Key).SelectMany(c => c).ToList());
 
 			_lstSelectedCurrencies.DisplayMember = "NameAndSymbol";
-			_lstSelectedCurrencies.Sorted = true;
 			_lstSelectedCurrencies.ValueMember = "Id";
 			_lstSelectedCurrencies.DataSource = _selectedCurrenciesDataSource;
 
 			_lstUnselectedCurrencies.DisplayMember = "NameAndSymbol";
-			_lstUnselectedCurrencies.Sorted = true;
 			_lstUnselectedCurrencies.ValueMember = "Id";
 			_lstUnselectedCurrencies.DataSource = _unselectedCurrenciesDataSource;
 		}
 
 		#region Event Handlers
 		private void OnBtnAddSelectedCurrenciesClick(object sender, System.EventArgs e) {
-			SetButtonStates(false);
-
-			foreach (var item in _lstUnselectedCurrencies.SelectedItems.Cast<Currency>().ToList()) {
-				_selectedCurrenciesDataSource.Add(item);
-				_unselectedCurrenciesDataSource.Remove(item);
-			}
-
-			SetButtonStates(true);
+			MoveSelectedCurrencies(_lstUnselectedCurrencies, ref _unselectedCurrenciesDataSource, _lstSelectedCurrencies, ref _selectedCurrenciesDataSource);
 		}
 
 		private void OnBtnRemoveSelectedCurrenciesClick(object sender, System.EventArgs e) {
-			SetButtonStates(false);
-
-			foreach (var item in _lstSelectedCurrencies.SelectedItems.Cast<Currency>().ToList()) {
-				_selectedCurrenciesDataSource.Remove(item);
-				_unselectedCurrenciesDataSource.Add(item);
-			}
-			
-			SetButtonStates(true);
+			MoveSelectedCurrencies(_lstSelectedCurrencies, ref _selectedCurrenciesDataSource, _lstUnselectedCurrencies, ref _unselectedCurrenciesDataSource);
 		}
 		#endregion
+
+		private void MoveSelectedCurrencies(ListBox sourceList, ref BindingList<Currency> sourceDataSource, ListBox destinationList, ref BindingList<Currency> destinationDataSource) {
+			var selectedIndices = sourceList.SelectedIndices.Cast<int>().ToArray();
+
+			if (selectedIndices.Length > 0) {
+				SetButtonStates(false);
+			
+				var tempDestination = new List<Currency>(destinationDataSource);
+				var tempSource = new List<Currency>(sourceDataSource);
+
+				for(var i = selectedIndices.Length - 1; i >= 0; i--) {
+					var currency = sourceDataSource[selectedIndices[i]];
+
+					tempSource.RemoveAt(selectedIndices[i]);
+					tempDestination.Add(currency);
+				}
+
+				destinationDataSource = new BindingList<Currency>(tempDestination.OrderBy(c => c.NameAndSymbol).ToList());
+				destinationList.DataSource = destinationDataSource;
+				sourceDataSource = new BindingList<Currency>(tempSource);
+				sourceList.DataSource = sourceDataSource;
+				sourceList.SelectedIndices.Clear();
+				sourceList.SelectedIndices.Add(selectedIndices[0] > 0 ? selectedIndices[0] - 1 : 0);
+
+				SetButtonStates(true);
+			}
+		} 
 
 		private void SetButtonStates(bool isEnabled) {
 			_btnAddSelectedCurrencies.Enabled = isEnabled;
@@ -65,9 +76,9 @@ namespace CryptoCurrencyMonitor.MarketData.Monitor {
 			_btnRemoveSelectedCurrencies.Enabled = isEnabled;
 		}
 
-		private readonly ICollection<int> _selectedCurrencyTypes;
+		private readonly IReadOnlyCollection<int> _selectedCurrencyTypes;
 		private BindingList<Currency> _selectedCurrenciesDataSource;
 		private BindingList<Currency> _unselectedCurrenciesDataSource;
-		private static readonly IReadOnlyCollection<Currency> AllCurrencies = CurrencyTypeRegistry.CurrencyTypeMap.Select(kv => new Currency(kv.Key) { Name = kv.Value.Item2, Symbol = kv.Value.Item1 }).ToList();
+		private static readonly IReadOnlyCollection<Currency> AllCurrencies = CurrencyTypeRegistry.CurrencyTypeMap.Select(kv => new Currency(kv.Key) { Name = kv.Value.Item2, Symbol = kv.Value.Item1 }).OrderBy(c => c.NameAndSymbol).ToList();
 	}
 }
