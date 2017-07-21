628b1e7bbd68327187bfb4f5a36ea05ba69d23b8
BikBikBikBikBik <BikBikBikBikBik@users.noreply.github.com>
2017-07-21 02:40:27 -0600

    Added license notice to all source files

--- CryptoCurrencyMonitor.MarketData.Monitor/Program.cs
+++ CryptoCurrencyMonitor.MarketData.Monitor/Program.cs
@@ -1,11 +1,26 @@
-﻿using System;
+﻿/*
+Copyright (C) 2017 BikBikBikBikBik
+
+This file is part of CryptoCurrencyMonitor.
+
+CryptoCurrencyMonitor is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+CryptoCurrencyMonitor is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with CryptoCurrencyMonitor.  If not, see <http://www.gnu.org/licenses/>.
+*/
+using System;
 using System.Windows.Forms;
 
 namespace CryptoCurrencyMonitor.MarketData.Monitor {
 	static class Program {
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
 		[STAThread]
 		static void Main() {
 			Application.EnableVisualStyles();
